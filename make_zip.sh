ee5acfc2a6ee5725b915d8c06336b79b9cc34353
Denilson Sá Maia <denilsonsa@gmail.com>
2024-06-03 23:32:47 +0200

    Independent manifest files for Chrome and Firefox
    Chrome Web Store rejected the latest version of this extension because
    of the manifest.
    
    > **Violation reference ID:** Yellow Magnesium
    >
    > **Violation:** Not providing promised functionality
    >
    > "The package is invalid as The "background.scripts" key cannot be used with manifest_version 3. Use the "background.service_worker" key instead."
    >
    > **How to rectify:** Test the code that you submit to the Web Store locally; verify that your submission contains the files that you expect at the paths that you expect and is working as intended. For more information, please refer to the documentation and migration checklist on Manifest V3 version.
    >
    > **Relevant section of the programme policy:** Extensions with broken functionality – such as dead sites or non-functioning features – are not allowed.
    
    So now I'm having two copies of the manifest file. One for Chrome (with
    service workers but without background scripts), another for Firefox
    (with background scripts but without service workers).

--- make_zip.sh
+++ make_zip.sh
@@ -1,24 +1,30 @@
 #!/bin/sh
 
-rm -f crx-uri-splitter.zip
-# -j junk (don't record) directory names
-zip -9Xj crx-uri-splitter.zip \
-	common.css \
-	common.js \
-	icon-dark-128.png \
-	icon-dark-16.png \
-	icon-dark-24.png \
-	icon-dark-32.png \
-	icon-dark-48.png \
-	icon-light-128.png \
-	icon-light-16.png \
-	icon-light-24.png \
-	icon-light-32.png \
-	icon-light-48.png \
-	manifest.json \
-	options.html \
-	options.js \
-	popup.html \
-	popup.js \
-	punycode-v1.4.1.js \
-	serviceworker.js
+rm -f crx-uri-splitter{,-chrome,-firefox}.zip
+
+for browser in chrome firefox ; do
+	cp -a "manifest-${browser}.json" "manifest.json"
+
+	# -j junk (don't record) directory names
+	zip -9Xj "crx-uri-splitter-${browser}.zip" \
+		common.css \
+		common.js \
+		icon-dark-128.png \
+		icon-dark-16.png \
+		icon-dark-24.png \
+		icon-dark-32.png \
+		icon-dark-48.png \
+		icon-light-128.png \
+		icon-light-16.png \
+		icon-light-24.png \
+		icon-light-32.png \
+		icon-light-48.png \
+		manifest.json \
+		options.html \
+		options.js \
+		popup.html \
+		popup.js \
+		punycode-v1.4.1.js \
+		serviceworker.js
+done
+rm manifest.json
