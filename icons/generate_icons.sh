2fabf296131909304121d4851fb6208d02b399a7
Denilson Sá Maia <denilsonsa@gmail.com>
2024-06-04 02:18:01 +0200

    Also calling zopflipng to optimize the PNG files
    The total size decreased from 120KB to 70KB.

--- icons/generate_icons.sh
+++ icons/generate_icons.sh
@@ -1,7 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+# Generates many PNG icon files based on a single SVG file.
+#
+# Note: this function would benefit from running in parallel.
 
 for color in white black gray lightgray darkgray dimgray ; do
 	for size in 16 24 32 48 64 128 256 ; do
-		cat scalable-icon.svg | sed 's/fill="black"/fill="'"${color}"'"/' | rsvg-convert --width $size --height $size --output "icon-${color}-${size}.png" -
+		name="icon-${color}-${size}.png"
+		cat scalable-icon.svg | sed 's/fill="black"/fill="'"${color}"'"/' | rsvg-convert --width $size --height $size --output "z${name}" -
+		zopflipng -y "z${name}" "${name}"
+		rm "z${name}"
 	done
 done
